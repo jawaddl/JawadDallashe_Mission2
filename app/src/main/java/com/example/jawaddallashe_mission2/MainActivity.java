5792d9bc3987e89dcbb7b772f39975c6c736e045
DESKTOP-FIA3LA0 <jawaddl2007@Gmail.com>
2024-01-08 23:17:51 +0200

    JawadDallashe_Mission2

--- app/src/main/java/com/example/jawaddallashe_mission2/MainActivity.java
+++ app/src/main/java/com/example/jawaddallashe_mission2/MainActivity.java
@@ -2,14 +2,35 @@ package com.example.jawaddallashe_mission2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
 
+    int count = 0 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //dd
+        btn = findViewById(R.id.button);
+        btn.setBackgroundColor(Color.CYAN);
+    }
+
+
+    public void go(View view) {
+        count++;
+        String str = count + " ";
+        btn.setText(str);
+        btn.setText("This is a click number:" + count);
+        btn.setTextSize(30);
+        btn.setTextColor(Color.BLACK);
+
+
     }
 }
